cfa29b88eb3e43f9fafa386b450fbc388d6bbbe8
omegacoo <bsumser@yahoo.com>
2020-03-18 20:48:23 -0700

    set up for remote server

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000'
+    API_ENDPOINT: 'postgres://ccjqyyvqrtrixn:a984a3fd9253ebdfe342421349445deb05b76c56edbde19b58f04740f4afe400@ec2-18-235-97-230.compute-1.amazonaws.com:5432/deu06qntv5kd9c'
 };
