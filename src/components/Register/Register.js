467be6fea438ac9a43800f9af7fdab4deb92989f
omegacoo <bsumser@yahoo.com>
2020-03-20 17:06:11 -0700

    registration page redesigned

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -6,7 +6,8 @@ export default class Register extends React.Component {
     state = {
         username: '',
         password: '',
-        confirmPassword: ''
+        confirmPassword: '',
+        email: ''
     };
 
     handleUsernameChange = e => {
@@ -26,44 +27,69 @@ export default class Register extends React.Component {
             confirmPassword: e.target.value
         });
     };
+    
+    handleEmailChange = e => {
+        this.setState({
+            email: e.target.value
+        });
+    };
 
     handleSubmit = e => {
         e.preventDefault();
+        console.log('implement registration!');
     };
 
     render(){
         return(
-            <form
-                className='Register'
-                onSubmit={this.handleSubmit}
-            >
-                <label htmlFor='Register_username'>Username: </label>
-                <input 
-                    className='Register_username'
-                    type='text'
-                    value={this.state.username}
-                    onChange={this.handleUsernameChange}
-                /><br/>
-                <label htmlFor='Register_password'>Password: </label>
-                <input 
-                    className='Register_password'
-                    type='password'
-                    value={this.state.password}
-                    onChange={this.handlePasswordChange}
-                /><br/>
-                <label htmlFor='Register_confirmPassword'>Confirm Password: </label>
-                <input 
-                    className='Register_confirmPassword'
-                    type='password'
-                    value={this.state.confirmPassword}
-                    onChange={this.handleConfirmPasswordChange}
-                />
-                <button 
-                    type='submit'
+            <>
+                <h1 className='Register_title'>You are on your way to some great meals! So close now...</h1>
+                <form
+                    className='Register'
+                    onSubmit={this.handleSubmit}
                 >
-                    Register
-                </button>
-            </form>
+                    <label htmlFor='Register_username'>Username: </label>
+                    <br/>
+                    <input 
+                        className='Register_username'
+                        type='text'
+                        value={this.state.username}
+                        onChange={this.handleUsernameChange}
+                    />
+                    <br/>
+                    <label htmlFor='Register_password'>Password: </label>
+                    <br/>
+                    <input 
+                        className='Register_password'
+                        type='password'
+                        value={this.state.password}
+                        onChange={this.handlePasswordChange}
+                    />
+                    <br/>
+                    <label htmlFor='Register_confirmPassword'>Confirm Password: </label>
+                    <br/>
+                    <input 
+                        className='Register_confirmPassword'
+                        type='password'
+                        value={this.state.confirmPassword}
+                        onChange={this.handleConfirmPasswordChange}
+                    />
+                    <br/>
+                    <label htmlFor='Register_email'>Email: </label>
+                    <br/>
+                    <input 
+                        className='Register_email'
+                        type='email'
+                        value={this.state.email}
+                        onChange={this.handleEmailChange}
+                    />
+                    <br/>
+                    <button 
+                        type='submit'
+                    >
+                        Register
+                    </button>
+                </form>
+            </>
         );
     };
 };
