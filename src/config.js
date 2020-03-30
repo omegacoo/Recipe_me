2b6f91e59391f50bb74636e666b6627392ff583d
omegacoo <bsumser@yahoo.com>
2020-03-30 11:14:37 -0700

    testing on local server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://quiet-tor-53875.herokuapp.com',
-    // API_ENDPOINT: 'http://localhost:8000'
+    // API_ENDPOINT: 'https://quiet-tor-53875.herokuapp.com',
+    API_ENDPOINT: 'http://localhost:8000'
 };
